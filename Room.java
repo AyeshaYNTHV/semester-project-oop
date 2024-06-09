2eb508ca04f4c5120b90e8e78c6a636d6c7bb1ff
iqrahafeez-2 <146338521+iqrahafeez-2@users.noreply.github.com>
2024-06-09 11:59:11 +0500

    Create Room.java

---
+++ Room.java
@@ -0,0 +1,25 @@
+package com.example.project;
+
+public class Room {
+    private int id;
+    private String name;
+    private String type;
+
+    public Room(int id, String name, String type) {
+        this.id = id;
+        this.name = name;
+        this.type = type;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+}
